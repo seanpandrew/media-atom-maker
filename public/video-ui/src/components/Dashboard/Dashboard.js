25b4dfdbcf8122b2442b51d38effafdf68ad6b7d
Michael Barton <michael.barton@guardian.co.uk>
2017-04-11 14:45:52 +0100

    Add old sk00l google image api graphs

--- public/video-ui/src/components/Dashboard/Dashboard.js
+++ public/video-ui/src/components/Dashboard/Dashboard.js
@@ -1,12 +1,69 @@
 import React from 'react';
+import Measure from 'react-measure';
 
-function Headline({right}) {
+class Graph extends React.Component {
+    renderGraph = ({width, height}) => {
+        if(width === 0 || height === 0) {
+            return <div className="dashboard__placeholder" />;
+        }
+
+        const series = this.props.data.map((s) => s.join(","));
+        const data = `chd=t:${series.join("|")}`;
+
+        const background = `chf=bg,s,333333`;
+        const colours = `chco=${this.props.colours.join(",")}`;
+
+        const dimensions = `chs=${Math.floor(width)}x${Math.floor(height)}`;
+        const margins = "chma=5,5,5,5";
+
+        const params = ["cht=lc:nda", "chds=a", dimensions, background, colours, margins, data];
+        const url = `https://chart.googleapis.com/chart?${params.join("&")}`;
+
+        return <img src={url} width={width} height={height} />;
+    };
+
+    render() {
+        return <Measure>{this.renderGraph}</Measure>;
+    }
+}
+
+function CostMetrics() {
     return <div className="dashboard__headline">
         <div className="dashboard__headline__left">
 
         </div>
         <div className="dashboard__headline__right">
-            {right.map((line) => <div key={line}>{line}</div>)}
+            <strong>$</strong>
+        </div>
+    </div>;
+}
+
+function PublishTimeMetrics({ today, metrics }) {
+    const avgTimeToday = Math.ceil(today.newRoute.avgPublishTime / 1000 / 60);
+    const avgTimeMins = metrics.map((day) => Math.ceil(day.newRoute.avgPublishTime / 1000 / 60));
+
+    return <div className="dashboard__headline">
+        <div className="dashboard__headline__left">
+            <Graph data={[avgTimeMins]} colours={["ffbc01"]} />
+        </div>
+        <div className="dashboard__headline__right">
+            <strong>{avgTimeToday}</strong>
+            <sub>mins</sub>
+        </div>
+    </div>;
+}
+
+function OldVsNewMetrics({ today, metrics }) {
+    const oldVideos = metrics.map((metric) => metric.oldRoute.videos);
+    const newVideos = metrics.map((metric) => metric.newRoute.videos);
+
+    return <div className="dashboard__headline">
+        <div className="dashboard__headline__left">
+            <Graph data={[newVideos, oldVideos]} colours={["148ad2", "ff7272"]} />
+        </div>
+        <div className="dashboard__headline__right">
+            <strong>{today.newRoute.videos}</strong>
+            <sub>{today.oldRoute.videos}</sub>
         </div>
     </div>;
 }
@@ -22,12 +79,12 @@ class Dashboard extends React.Component {
         }
 
         const today = this.props.metrics.dayMetrics[0];
-        const avgTime = Math.ceil(today.newRoute.avgPublishTime / 1000 / 60);
+        const metrics = this.props.metrics.dayMetrics.slice().reverse();
 
         return <div className="dashboard__top">
-            <Headline right={[today.newRoute.videos, today.oldRoute.videos]} />
-            <Headline right={[avgTime, "mins"]} />
-            <Headline right={["€"]} />
+            <OldVsNewMetrics today={today} metrics={metrics} />
+            <PublishTimeMetrics today={today} metrics={metrics} />
+            <CostMetrics />
         </div>;
     }
 
