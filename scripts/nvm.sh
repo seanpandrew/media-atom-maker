7ea34e1ba65d7b90b29ddaa1e451e22ed38ca107
Akash A <akash1810@gmail.com>
2017-05-16 08:59:52 +0100

    automatically use nvm if available

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -28,10 +28,3 @@ if nvm_installed; then
   fi
   nvm install
 fi
-
-printf "\n\rRemoving compiled css file... \n\r\n\r"
-rm public/video-ui/build/main.css 2> /dev/null
-printf "\n\rStarting Webpack Dev Server... \n\r\n\r"
-yarn run client-dev &
-printf "\n\rStarting Play App... \n\r\n\r"
-JS_ASSET_HOST=https://video-assets.local.dev-gutools.co.uk/assets/ ./sbt $@ app/run
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,27 @@ installed() {
   hash "$1" 2>/dev/null
 }
 
+nvm_installed() {
+  if [ -d '/usr/local/opt/nvm' ] || [ -d "$HOME/.nvm" ]; then
+    true
+  else
+    false
+  fi
+}
+
+nvm_available() {
+  type -t nvm > /dev/null
+}
+
+source_nvm() {
+  if ! nvm_available; then
+    [ -e "/usr/local/opt/nvm/nvm.sh" ] && source /usr/local/opt/nvm/nvm.sh
+  fi
+  if ! nvm_available; then
+    [ -e "$HOME/.nvm/nvm.sh" ] && source $HOME/.nvm/nvm.sh
+  fi
+}
+
 install_yarn() {
   if ! installed yarn; then
     echo 'Installing yarn'
@@ -20,6 +41,14 @@ install_deps_and_build() {
 }
 
 main() {
+  # if nvm is installed, install the node version required and use it
+  if nvm_installed; then
+    if ! nvm_available; then
+      source_nvm
+    fi
+    nvm install
+  fi
+
   install_yarn
   install_deps_and_build
   printf "\n\rDone.\n\r\n\r"
